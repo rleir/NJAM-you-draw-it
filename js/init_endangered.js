d4a48729cac6ab778ab06e01c9068072911631df
Rick Leir <rleir@leirtech.com>
2019-10-16 12:08:13 -0400

    do not use 'house', use 'endangered'

--- js/init_endangered.js
+++ js/init_endangered.js
@@ -1,25 +1,25 @@
 var pymChild = new pym.Child();
 
 var data = [
-    {'year': 2001, 'house': 115},
-    {'year': 2002, 'house': 125},
-    {'year': 2003, 'house': 153},
-    {'year': 2004, 'house': 169},
-    {'year': 2005, 'house': 184},
-    {'year': 2006, 'house': 205},
-    {'year': 2007, 'house': 222},
-    {'year': 2008, 'house': 234},
-    {'year': 2009, 'house': 244},
-    {'year': 2010, 'house': 262},
-    {'year': 2011, 'house': 278},
-    {'year': 2012, 'house': 287},
-    {'year': 2013, 'house': 298},
-    {'year': 2014, 'house': 306},
-    {'year': 2015, 'house': 316},
-    {'year': 2016, 'house': 320},
-    {'year': 2017, 'house': 321},
-    {'year': 2018, 'house': 338},
-    {'year': 2019, 'house': 356}
+    {'year': 2001, 'endangered': 115},
+    {'year': 2002, 'endangered': 125},
+    {'year': 2003, 'endangered': 153},
+    {'year': 2004, 'endangered': 169},
+    {'year': 2005, 'endangered': 184},
+    {'year': 2006, 'endangered': 205},
+    {'year': 2007, 'endangered': 222},
+    {'year': 2008, 'endangered': 234},
+    {'year': 2009, 'endangered': 244},
+    {'year': 2010, 'endangered': 262},
+    {'year': 2011, 'endangered': 278},
+    {'year': 2012, 'endangered': 287},
+    {'year': 2013, 'endangered': 298},
+    {'year': 2014, 'endangered': 306},
+    {'year': 2015, 'endangered': 316},
+    {'year': 2016, 'endangered': 320},
+    {'year': 2017, 'endangered': 321},
+    {'year': 2018, 'endangered': 338},
+    {'year': 2019, 'endangered': 356}
 ]
 
 var winwidth = parseInt(d3.select('#chart-body-3').style('width'))
@@ -53,8 +53,8 @@ c.y.domain([0, 400])
 c.xAxis.ticks(4).tickFormat(ƒ())
 c.yAxis.ticks(5).tickFormat(d => d )
 
-var area = d3.area().x(ƒ('year', c.x)).y0(ƒ('house', c.y)).y1(c.height)
-var line = d3.area().x(ƒ('year', c.x)).y(ƒ('house', c.y))
+var area = d3.area().x(ƒ('year', c.x)).y0(ƒ('endangered', c.y)).y1(c.height)
+var line = d3.area().x(ƒ('year', c.x)).y(ƒ('endangered', c.y))
 
 var clipRect = c.svg
   .append('clipPath#clip')
@@ -63,14 +63,14 @@ var clipRect = c.svg
 
 var correctSel = c.svg.append('g').attr('clip-path', 'url(#clip)')
 
-correctSel.append('path.area.house-area').at({d: area(data)})
+correctSel.append('path.area.endangered-area').at({d: area(data)})
 correctSel.append('path.line').at({d: line(data)})
-yourDataSel = c.svg.append('path#your-line-3').attr('class', 'your-line house-line')
+yourDataSel = c.svg.append('path#your-line-3').attr('class', 'your-line endangered-line')
 
 c.drawAxis()
 
 yourData = data
-  .map(function(d){  return {year: d.year, house: d.house, defined: 0} })
+  .map(function(d){  return {year: d.year, endangered: d.endangered, defined: 0} })
   .filter(function(d){
     if (d.year == 2005) d.defined = true
     return d.year >= 2005
@@ -83,11 +83,11 @@ var drag = d3.drag()
     d3.selectAll('.intro-text').style('opacity', 0)
     var pos = d3.mouse(this)
     var year = clamp(2005, 2019, c.x.invert(pos[0]))
-    var house = clamp(0, c.y.domain()[1], c.y.invert(pos[1]))
+    var endangered = clamp(0, c.y.domain()[1], c.y.invert(pos[1]))
 
     yourData.forEach(function(d){
       if (Math.abs(d.year - year) < .5){
-        d.house = house
+        d.endangered = endangered
         d.defined = true
       }
     })
@@ -99,7 +99,7 @@ var drag = d3.drag()
       clipRect.transition().duration(1000).attr('width', c.x(2019))
         d3.select('#answer-3')
             .style('visibility', 'visible')
-            .html("<div>You guessed <p class='your-pink'>"+ d3.format(",.3r")(yourData[yourData.length-1].house) + "</p> for 2019.</div><div>The real value was <p class='your-pink'>"+d3.format(",.3r")(data[18].house)+"</p>.</div>")
+            .html("<div>You guessed <p class='your-pink'>"+ d3.format(",.3r")(yourData[yourData.length-1].endangered) + "</p> for 2019.</div><div>The real value was <p class='your-pink'>"+d3.format(",.3r")(data[18].endangered)+"</p>.</div>")
       d3.select('#explain-3').style('visibility', 'visible').style('opacity', 1)
           pymChild.sendHeight();
 
